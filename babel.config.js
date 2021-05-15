08b03476c8d7f3b92f4101a51a545472d55405b7
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-05-15 22:35:33 +0900

    18. Installing and Setting Up a Module Resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ["babel-preset-expo"]
+        presets: ["babel-preset-expo"],
+        plugins: [
+            ["module-resolver" , {
+                alias : {
+                    "@screens" : "./src/screens",
+                    "@components" : "./src/components",
+                    "@utiles" : "./src/utiles",
+                    "@contexts" : "./src/contexts",
+                    "@config" : "./src/config",
+                    "@assets" : "./assets"
+                }
+            }]
+        ]
     };
 };
