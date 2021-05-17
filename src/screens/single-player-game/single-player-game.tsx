d5e41ca94738821320c478873099e54b5ffed380
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-05-18 01:08:57 +0900

    28. More Utility Functions

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -2,13 +2,22 @@ import React, { ReactElement } from "react";
 import { SafeAreaView } from "react-native";
 import { GradientBackground, Board } from "@components";
 
-import { printFormattedBoard, BoardState } from "@utils";
+import {
+    printFormattedBoard,
+    isEmpty,
+    isFull,
+    getAvailableMoves,
+    BoardState
+} from "@utils";
 
 import styles from "./single-player-game.style";
 
 export default function SinglePlayerGame(): ReactElement {
     const b: BoardState = ["x", "o", null, "x", "o", null, "x", "o", null];
     printFormattedBoard(b);
+    console.log(isEmpty(b));
+    console.log(isFull(b));
+    console.log(getAvailableMoves(b));
     return (
         <GradientBackground>
             <SafeAreaView style={styles.container}>
