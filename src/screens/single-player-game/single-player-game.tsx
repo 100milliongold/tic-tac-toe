10621be81ab0e6a4864ad0c13ba0d6ad6f915995
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-05-20 22:30:54 +0900

    39. Refactoring our Sounds Code into a Custom Hook

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,11 +1,16 @@
-import React, { ReactElement, useState, useEffect, useRef } from "react";
+import React, { ReactElement, useState, useEffect } from "react";
 import { SafeAreaView } from "react-native";
 import { GradientBackground, Board } from "@components";
 import styles from "./single-player-game.style";
 
-import { isEmpty, BoardState, isTerminal, getBestMove, Cell } from "@utils";
-import { Audio } from "expo-av";
-import * as Haptics from "expo-haptics";
+import {
+    isEmpty,
+    BoardState,
+    isTerminal,
+    getBestMove,
+    Cell,
+    useSounds
+} from "@utils";
 
 export default function SinglePlayerGame(): ReactElement {
     // prettier-ignore
@@ -23,11 +28,7 @@ export default function SinglePlayerGame(): ReactElement {
     // 인간이 셀 선택항목이 가장 많을때 -> 인간이 먼저 선택했는지 여부
     const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
 
-    const popSoundref = useRef<Audio.Sound | null>(null);
-    const pop2Soundref = useRef<Audio.Sound | null>(null);
-    const winSoundref = useRef<Audio.Sound | null>(null);
-    const lossSoundref = useRef<Audio.Sound | null>(null);
-    const drawSoundref = useRef<Audio.Sound | null>(null);
+    const playSound = useSounds();
 
     const insertCell = (cell: number, symbol: "x" | "o"): void => {
         const stateCopy: BoardState = [...state];
@@ -35,10 +36,7 @@ export default function SinglePlayerGame(): ReactElement {
         stateCopy[cell] = symbol;
         setState(stateCopy);
         try {
-            symbol === "x"
-                ? popSoundref.current?.replayAsync()
-                : pop2Soundref.current?.replayAsync();
-            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+            symbol === "x" ? playSound("pop1") : playSound("pop2");
         } catch (error) {
             console.log(error);
         }
@@ -69,36 +67,15 @@ export default function SinglePlayerGame(): ReactElement {
             // handle game finished
             const winner = getWinner(gameResult.winner);
             if (winner === "HUMAN") {
-                try {
-                    winSoundref.current?.replayAsync();
-                    Haptics.notificationAsync(
-                        Haptics.NotificationFeedbackType.Success
-                    );
-                } catch (error) {
-                    console.log(error);
-                }
+                playSound("win");
                 alert("You Won!");
             }
             if (winner === "BOT") {
-                try {
-                    lossSoundref.current?.replayAsync();
-                    Haptics.notificationAsync(
-                        Haptics.NotificationFeedbackType.Error
-                    );
-                } catch (error) {
-                    console.log(error);
-                }
+                playSound("loss");
                 alert("You Lost!");
             }
             if (winner === "DRAW") {
-                try {
-                    drawSoundref.current?.replayAsync();
-                    Haptics.notificationAsync(
-                        Haptics.NotificationFeedbackType.Warning
-                    );
-                } catch (error) {
-                    console.log(error);
-                }
+                playSound("draw");
                 alert("It's a Draw!");
             }
         } else {
@@ -120,47 +97,6 @@ export default function SinglePlayerGame(): ReactElement {
             }
         }
     }, [state, turn]);
-
-    useEffect(() => {
-        //여기에서 사운드를 로드
-        const popSoundObject = new Audio.Sound();
-        const pop2SoundObject = new Audio.Sound();
-        const winSoundObject = new Audio.Sound();
-        const lossSoundObject = new Audio.Sound();
-        const drawSoundObject = new Audio.Sound();
-
-        const loadSounds = async () => {
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            await popSoundObject.loadAsync(require("@assets/pop_1.wav"));
-            popSoundref.current = popSoundObject;
-
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
-            pop2Soundref.current = pop2SoundObject;
-
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            await winSoundObject.loadAsync(require("@assets/win.mp3"));
-            winSoundref.current = winSoundObject;
-
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            await lossSoundObject.loadAsync(require("@assets/loss.mp3"));
-            lossSoundref.current = lossSoundObject;
-
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            await drawSoundObject.loadAsync(require("@assets/draw.mp3"));
-            drawSoundref.current = drawSoundObject;
-        };
-        loadSounds();
-
-        return () => {
-            // 여기에서 사운드를 헤제
-            popSoundObject && popSoundObject.unloadAsync();
-            pop2SoundObject && pop2SoundObject.unloadAsync();
-            winSoundObject && winSoundObject.unloadAsync();
-            lossSoundObject && lossSoundObject.unloadAsync();
-            drawSoundObject && drawSoundObject.unloadAsync();
-        };
-    }, []);
 
     return (
         <GradientBackground>
---
+++ src/utils/useSounds.ts
@@ -0,0 +1,97 @@
+import { useRef, useEffect } from "react";
+import { Audio } from "expo-av";
+import * as Haptics from "expo-haptics";
+
+type SoundType = "pop1" | "pop2" | "win" | "loss" | "draw";
+
+export default function useSounds(): (sound: SoundType) => void {
+    const popSoundref = useRef<Audio.Sound | null>(null);
+    const pop2Soundref = useRef<Audio.Sound | null>(null);
+    const winSoundref = useRef<Audio.Sound | null>(null);
+    const lossSoundref = useRef<Audio.Sound | null>(null);
+    const drawSoundref = useRef<Audio.Sound | null>(null);
+
+    const playSound = async (sound: SoundType): Promise<void> => {
+        const soundMap = {
+            pop1: popSoundref,
+            pop2: pop2Soundref,
+            win: winSoundref,
+            loss: lossSoundref,
+            draw: drawSoundref
+        };
+        try {
+            const status = await soundMap[sound].current?.getStatusAsync();
+            status && status.isLoaded && soundMap[sound].current?.replayAsync();
+            switch (sound) {
+                case "pop1":
+                case "pop2":
+                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                    break;
+                case "win":
+                    Haptics.notificationAsync(
+                        Haptics.NotificationFeedbackType.Success
+                    );
+                    break;
+                case "loss":
+                    Haptics.notificationAsync(
+                        Haptics.NotificationFeedbackType.Error
+                    );
+                    break;
+                case "draw":
+                    Haptics.notificationAsync(
+                        Haptics.NotificationFeedbackType.Warning
+                    );
+
+                    break;
+
+                default:
+                    break;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        //여기에서 사운드를 로드
+        const popSoundObject = new Audio.Sound();
+        const pop2SoundObject = new Audio.Sound();
+        const winSoundObject = new Audio.Sound();
+        const lossSoundObject = new Audio.Sound();
+        const drawSoundObject = new Audio.Sound();
+
+        const loadSounds = async () => {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            await popSoundObject.loadAsync(require("@assets/pop_1.wav"));
+            popSoundref.current = popSoundObject;
+
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
+            pop2Soundref.current = pop2SoundObject;
+
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            await winSoundObject.loadAsync(require("@assets/win.mp3"));
+            winSoundref.current = winSoundObject;
+
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            await lossSoundObject.loadAsync(require("@assets/loss.mp3"));
+            lossSoundref.current = lossSoundObject;
+
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            await drawSoundObject.loadAsync(require("@assets/draw.mp3"));
+            drawSoundref.current = drawSoundObject;
+        };
+        loadSounds();
+
+        return () => {
+            // 여기에서 사운드를 헤제
+            popSoundObject && popSoundObject.unloadAsync();
+            pop2SoundObject && pop2SoundObject.unloadAsync();
+            winSoundObject && winSoundObject.unloadAsync();
+            lossSoundObject && lossSoundObject.unloadAsync();
+            drawSoundObject && drawSoundObject.unloadAsync();
+        };
+    }, []);
+
+    return playSound;
+}
