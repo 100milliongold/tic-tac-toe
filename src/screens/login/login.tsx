fac71589de98d0afdd5a0930879acd4231e33151
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-05-22 23:08:50 +0900

    60. Creating a Custom TextInput Component with Ref Forwarding

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,26 +1,29 @@
-import React, { ReactElement } from "react";
-import { ScrollView, TextInput } from "react-native";
+import React, { ReactElement, useRef } from "react";
+import { ScrollView, TextInput as NativeTextInput } from "react-native";
 import styles from "./login.sytles";
-import { GradientBackground } from "@components";
+import { GradientBackground, TextInput } from "@components";
 import { colors } from "@utils";
 
 export default function Login(): ReactElement {
+    const passwordRef = useRef<NativeTextInput | null>(null);
     return (
         <GradientBackground>
             <ScrollView contentContainerStyle={styles.container}>
                 <TextInput
+                    returnKeyType="next"
                     placeholder="Username"
-                    placeholderTextColor="#5d5379"
                     style={{
-                        height: 50,
-                        width: "100%",
-                        borderBottomWidth: 1,
-                        borderColor: colors.lightGreen,
-                        backgroundColor: colors.purple,
-                        padding: 10,
-                        color: colors.lightGreen,
-                        fontFamily: "DeliusUnicase_400Regular"
+                        marginBottom: 20
                     }}
+                    onSubmitEditing={() => {
+                        passwordRef.current?.focus();
+                    }}
+                />
+                <TextInput
+                    ref={passwordRef}
+                    secureTextEntry
+                    returnKeyType="done"
+                    placeholder="Password"
                 />
             </ScrollView>
         </GradientBackground>
