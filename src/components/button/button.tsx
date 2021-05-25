3ac0fc7047822cdf3705cd56ebc1f8221cbda46e
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-05-25 23:24:28 +0900

    61. Creating the Login Functionality

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,19 +1,23 @@
 import React, { ReactElement } from "react";
-import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Text, TouchableOpacity, TouchableOpacityProps, ActivityIndicator } from "react-native";
 import styles from "./button.style";
 
 type ButtonProps = {
     title: string;
+    loading: boolean
 } & TouchableOpacityProps;
 
 export default function Button({
     title,
     style,
+    loading,
     ...props
 }: ButtonProps): ReactElement {
     return (
-        <TouchableOpacity style={[styles.button, style]} {...props}>
-            <Text style={styles.buttonText}>{title}</Text>
+        <TouchableOpacity disabled={loading} style={[styles.button, style]} {...props}>
+            {loading ? <ActivityIndicator color="#000" /> :
+                <Text style={styles.buttonText}>{title}</Text>
+            }
         </TouchableOpacity>
     );
 }
--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,17 +1,47 @@
-import React, { ReactElement, useRef } from "react";
-import { ScrollView, TextInput as NativeTextInput } from "react-native";
+import React, { ReactElement, useRef, useState } from "react";
+import { Alert, ScrollView, TextInput as NativeTextInput } from "react-native";
 import styles from "./login.sytles";
-import { GradientBackground, TextInput } from "@components";
+import { GradientBackground, TextInput, Button } from "@components";
+import { Auth } from "aws-amplify";
 import { colors } from "@utils";
 
 export default function Login(): ReactElement {
     const passwordRef = useRef<NativeTextInput | null>(null);
+    const [form, setForm] = useState({
+        username: "",
+        password: ""
+    })
+    const [loading, setLoading] = useState(false)
+    const setFormInput = (key: keyof typeof form , value: string) : void => {
+        setForm({...form , [key] : value})
+    }
+
+    const login = async () => {
+        setLoading(true)
+        const { username , password} = form
+        console.log(username , password);
+
+        try {
+            const res = await Auth.signIn(username , password)
+            console.log(res);    
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Error!" , error.message || "An error occurred!")
+        }
+        
+        setLoading(false)
+    }
+
     return (
         <GradientBackground>
             <ScrollView contentContainerStyle={styles.container}>
                 <TextInput
                     returnKeyType="next"
                     placeholder="Username"
+                    value={form.username}
+                    onChangeText={(value) => {
+                        setFormInput("username" , value)
+                    }}
                     style={{
                         marginBottom: 20
                     }}
@@ -21,10 +51,18 @@ export default function Login(): ReactElement {
                 />
                 <TextInput
                     ref={passwordRef}
+                    value={form.password}
+                    onChangeText={(value) => {
+                        setFormInput("password" , value)
+                    }}
+                    style={{
+                        marginBottom : 30
+                    }}
                     secureTextEntry
                     returnKeyType="done"
                     placeholder="Password"
                 />
+                <Button loading={loading} title="Login" onPress={login}/>
             </ScrollView>
         </GradientBackground>
     );
