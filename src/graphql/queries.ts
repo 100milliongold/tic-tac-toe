01b66a8d7c2601507381670e123b79e63568b5a1
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-06-08 23:16:58 +0900

    79. Creating the PlayerGame Intermediate Model

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -2,44 +2,109 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getPlayer = /* GraphQL */ `
-    query GetPlayer($username: String!) {
-        getPlayer(username: $username) {
-            id
-            cognitoID
-            username
-            name
-            email
-            createdAt
-            updatedAt
+export const getGame = /* GraphQL */ `
+  query GetGame($id: ID!) {
+    getGame(id: $id) {
+      id
+      status
+      owners
+      initiator
+      turn
+      state
+      winner
+      players {
+        items {
+          id
+          createdAt
+          gameID
+          playerUsername
+          owners
+          updatedAt
         }
+        nextToken
+      }
+      createdAt
+      updatedAt
     }
+  }
+`;
+export const listGames = /* GraphQL */ `
+  query ListGames(
+    $filter: ModelGameFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listGames(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        status
+        owners
+        initiator
+        turn
+        state
+        winner
+        players {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getPlayer = /* GraphQL */ `
+  query GetPlayer($username: String!) {
+    getPlayer(username: $username) {
+      id
+      cognitoID
+      username
+      name
+      email
+      games {
+        items {
+          id
+          createdAt
+          gameID
+          playerUsername
+          owners
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
 `;
 export const listPlayers = /* GraphQL */ `
-    query ListPlayers(
-        $username: String
-        $filter: ModelPlayerFilterInput
-        $limit: Int
-        $nextToken: String
-        $sortDirection: ModelSortDirection
+  query ListPlayers(
+    $username: String
+    $filter: ModelPlayerFilterInput
+    $limit: Int
+    $nextToken: String
+    $sortDirection: ModelSortDirection
+  ) {
+    listPlayers(
+      username: $username
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      sortDirection: $sortDirection
     ) {
-        listPlayers(
-            username: $username
-            filter: $filter
-            limit: $limit
-            nextToken: $nextToken
-            sortDirection: $sortDirection
-        ) {
-            items {
-                id
-                cognitoID
-                username
-                name
-                email
-                createdAt
-                updatedAt
-            }
-            nextToken
+      items {
+        id
+        cognitoID
+        username
+        name
+        email
+        games {
+          nextToken
         }
+        createdAt
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
