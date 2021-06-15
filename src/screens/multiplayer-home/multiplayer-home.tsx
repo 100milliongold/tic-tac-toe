810395ec56e3fdd490edeaf3f5fd6c32732bf035
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-06-15 21:40:39 +0900

    94. Calling our GraphQL Query in the Multiplayer Home Screen

--- src/screens/multiplayer-home/multiplayer-home.tsx
+++ src/screens/multiplayer-home/multiplayer-home.tsx
@@ -1,12 +1,37 @@
-import React, { ReactElement } from "react";
-import { ScrollView, View } from "react-native";
+import React, { ReactElement, useEffect } from "react";
+import { Alert, ScrollView, View } from "react-native";
 import { GradientBackground, Text } from "@components";
 import styles from "./multiplayer-home.style";
 import { useAuth } from "@contexts/auth-context";
 import { colors } from "@utils";
+import { getPlayer } from "./multiplayer-home.graphql";
+import { API, graphqlOperation } from "aws-amplify";
 
 export default function MultiplayerHome(): ReactElement {
     const { user } = useAuth();
+
+    const fetchPlayer = async (nextToken: string | null) => {
+        if (user) {
+            try {
+                const player = await API.graphql(
+                    graphqlOperation(getPlayer, {
+                        username: user.username,
+                        limit: 1,
+                        sortDirection: "DESC",
+                        nextToken: nextToken
+                    })
+                );
+                console.log(player);
+            } catch (error) {
+                Alert.alert("Error!", "An error has occurrend!");
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchPlayer(null);
+    }, []);
+
     return (
         <GradientBackground>
             {user ? (
