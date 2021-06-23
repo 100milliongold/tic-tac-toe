7208212a14d3f9e17513f23c8bb7a72326ed3151
je.kim <gadian88@gmail.com>
2021-06-23 15:49:12 +0900

    102. Listening to Game Updates Using the onUpdateGameById Subscription

--- src/screens/multiplayer-home/game-item.tsx
+++ src/screens/multiplayer-home/game-item.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement, useEffect } from "react";
 import { TouchableOpacity } from "react-native";
-import { PlayerGameType } from "./multiplayer-home.graphql";
+import { PlayerGameType, onUpdateGameById } from "./multiplayer-home.graphql";
 import { Text } from "@components";
 import { useAuth } from "@contexts/auth-context";
 import { colors } from "@utils";
 import styles from "./multiplayer-home.style";
+import { API, graphqlOperation } from "aws-amplify";
+import Observable from "zen-observable";
 
 export default function GameItem({
     playerGame
@@ -36,10 +38,22 @@ export default function GameItem({
     );
 
     useEffect(() => {
-        if (game) {
-            console.log("mount", game.id);
+        if (game && (game.status === "REQUESTED" || game.status === "ACTIVE")) {
+            // graphql 에서 subscription 시 Observable 형태로 반환이 됨
+            // zen-observable 을 이용히여 Promise<GraphQLResult<object>> 을 Observable 으로 오버라이딩
+            const gameUpdates = API.graphql(
+                graphqlOperation(onUpdateGameById, {
+                    id: game.id
+                })
+            ) as unknown as Observable<{ [key: string]: any }>;
+            const subscription = gameUpdates.subscribe({
+                next: ({ value }) => {
+                    console.log("value: ", value);
+                }
+            });
+
             return () => {
-                console.log("umount", game.id);
+                subscription.unsubscribe();
             };
         }
     }, []);
