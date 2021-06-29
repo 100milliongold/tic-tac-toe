c0307559c2a76b8055bc4fbb61f49ac2ae92866c
je.kim <gadian88@gmail.com>
2021-06-29 15:20:43 +0900

    110. Fetching an Existing Game or Creating a New Game Based on Navigation Params

--- src/API.ts
+++ src/API.ts
@@ -2,42 +2,6 @@
 /* eslint-disable */
 //  This file was automatically generated and should not be edited.
 
-export enum ModelSortDirection {
-  ASC = "ASC",
-  DESC = "DESC",
-}
-
-
-export type Player = {
-  __typename: "Player",
-  id: string,
-  cognitoID: string,
-  username: string,
-  name: string,
-  email: string,
-  createdAt: string,
-  updatedAt: string,
-  games?: ModelPlayerGameConnection | null,
-};
-
-export type ModelPlayerGameConnection = {
-  __typename: "ModelPlayerGameConnection",
-  items?:  Array<PlayerGame | null > | null,
-  nextToken?: string | null,
-};
-
-export type PlayerGame = {
-  __typename: "PlayerGame",
-  id: string,
-  createdAt: string,
-  gameID: string,
-  playerUsername: string,
-  owners: Array< string >,
-  updatedAt: string,
-  player: Player,
-  game: Game,
-};
-
 export type Game = {
   __typename: "Game",
   id: string,
@@ -66,6 +30,109 @@ export enum Symbol {
   o = "o",
 }
 
+
+export type ModelPlayerGameConnection = {
+  __typename: "ModelPlayerGameConnection",
+  items?:  Array<PlayerGame | null > | null,
+  nextToken?: string | null,
+};
+
+export type PlayerGame = {
+  __typename: "PlayerGame",
+  id: string,
+  createdAt: string,
+  gameID: string,
+  playerUsername: string,
+  owners: Array< string >,
+  updatedAt: string,
+  player: Player,
+  game: Game,
+};
+
+export type Player = {
+  __typename: "Player",
+  id: string,
+  cognitoID: string,
+  username: string,
+  name: string,
+  email: string,
+  createdAt: string,
+  updatedAt: string,
+  games?: ModelPlayerGameConnection | null,
+};
+
+export type GameData = {
+  __typename: "GameData",
+  id: string,
+  status: GameStatus,
+  turn: string,
+  state: Array< Symbol | null >,
+  winner?: string | null,
+};
+
+export enum ModelSortDirection {
+  ASC = "ASC",
+  DESC = "DESC",
+}
+
+
+export type SearchablePlayerConnection = {
+  __typename: "SearchablePlayerConnection",
+  items?:  Array<Player | null > | null,
+  nextToken?: string | null,
+  total?: number | null,
+};
+
+export type getGameQueryVariables = {
+  id: string,
+};
+
+export type getGameQuery = {
+  getGame?:  {
+    __typename: "Game",
+    id: string,
+    status: GameStatus,
+    owners: Array< string >,
+    initiator: string,
+    turn: string,
+    state: Array< Symbol | null >,
+    winner?: string | null,
+    players?:  {
+      __typename: "ModelPlayerGameConnection",
+      items?:  Array< {
+        __typename: "PlayerGame",
+        player:  {
+          __typename: "Player",
+          username: string,
+          name: string,
+        },
+      } | null > | null,
+    } | null,
+  } | null,
+};
+
+export type startGameMutationVariables = {
+  invitee: string,
+};
+
+export type startGameMutation = {
+  startGame?:  {
+    __typename: "GameData",
+    id: string,
+  } | null,
+};
+
+export type playMoveMutationVariables = {
+  game: string,
+  index: number,
+};
+
+export type playMoveMutation = {
+  playMove?:  {
+    __typename: "GameData",
+    id: string,
+  } | null,
+};
 
 export type GetPlayerQueryVariables = {
   username: string,
@@ -109,7 +176,7 @@ export type GetPlayerQuery = {
 };
 
 export type onUpdateGameByIdSubscriptionVariables = {
-  id?: string,
+  id: string,
 };
 
 export type onUpdateGameByIdSubscription = {
@@ -123,7 +190,6 @@ export type onUpdateGameByIdSubscription = {
   } | null,
 };
 
-
 export type searchPlayersQueryVariables = {
   limit?: number | null,
   nextToken?: string | null,
@@ -131,13 +197,13 @@ export type searchPlayersQueryVariables = {
 };
 
 export type searchPlayersQuery = {
-  searchPlayers:  {
+  searchPlayers?:  {
     __typename: "SearchablePlayerConnection",
-    items:  Array< {
+    items?:  Array< {
       __typename: "Player",
       name: string,
       username: string,
     } | null > | null,
-    nextToken: string | null,
+    nextToken?: string | null,
   } | null,
 };
